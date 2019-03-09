28d4fbb2717c4c49400482a0b2436dbbbae2a1fc
chs <tianzhong_work@msn.com>
2019-03-09 19:03:22 +0800

    基本完工，测试通过

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -17,8 +17,8 @@ namespace ConsoleTest
             //ITrigger trigger = new RepeatTrigger(new TimeSpan(0, 0, 5), DateTime.Parse("12:50:00"), 10);
             //ITrigger trigger = new SingleTrigger(DateTime.Parse("12:47:00"));
             //ITrigger trigger = new SingleTrigger(DateTime.Parse("12:59:00"),5, new TimeSpan(0, 0, 5));
-            ITrigger trigger = new SingleTrigger(DateTime.Parse("12:59:00"), DateTime.Parse("13:59:00"), 5, new TimeSpan(0, 0, 5));
-            ITrigger trigger2 = new SingleTrigger(DateTime.Parse("12:59:00"), DateTime.Parse("13:59:00"), 5, new TimeSpan(0, 0, 5));
+            ITrigger trigger = new SingleTrigger(DateTime.Parse("12:59:00"), DateTime.Parse("19:59:00"), 5, new TimeSpan(0, 0, 5));
+            ITrigger trigger2 = new SingleTrigger(DateTime.Parse("12:59:00"), DateTime.Parse("19:59:00"), 5, new TimeSpan(0, 0, 5));
             schedule.Add(job, trigger);
             IJob job2 = new TestJob("test2",new IJob[] { job});
             
@@ -40,7 +40,7 @@ namespace ConsoleTest
         public override bool Execute()
         {
             Console.WriteLine("Job <{0}> run a time", this.Name);
-            return true;
+            return false;
         }
     }
 }
---
+++ JobSchedule/JobQueue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace HuaQuant.JobSchedule
+{
+    public class JobQueue : IJob
+    {
+        private string name = "JobQueue";
+        public string Name => this.name;
+
+        bool showDetail = false;
+        public bool ShowDetail
+        {
+            get => this.showDetail;
+            set => this.showDetail = value;
+        }
+        private IEnumerable<IJob> needJobs = null;
+        public IEnumerable<IJob> NeedJobs => this.needJobs;
+        private int frequencies = 0;
+        public int Frequencies => this.frequencies;
+
+        private List<IJob> jobs = new List<IJob>();
+        public JobQueue(string name, IEnumerable<IJob> needJobs = null, bool showDetail = false)
+        {
+            this.name = name;
+            this.needJobs = needJobs;
+            this.showDetail = showDetail;
+        }
+        public bool Execute()
+        {
+            bool ret = true;
+            foreach (IJob job in this.jobs)
+            {
+                ret = ret && job.Execute();
+            }
+            return ret;
+        }
+
+        public void IncrementFrequency()
+        {
+            Interlocked.Increment(ref frequencies);
+        }
+
+        public void AddJob(IJob job)
+        {
+            lock (this.jobs)
+            {
+                this.jobs.Add(job);
+            }
+        }
+    }
+}
